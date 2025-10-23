4864429444756a8c31e96e685531de16b70beff5
svdcvt <diksik007@gmail.com>
2025-10-23 13:56:17 +0000

    Deploying to gh-pages from @ svdcvt/svdcvt.github.io@e49b63dcc86458b26bdf408c715ffadb8803d1b8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: ".",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
