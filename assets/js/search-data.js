764d3148632a419c5ef32443044066b8b036c86f
svdcvt <diksik007@gmail.com>
2025-10-23 13:32:09 +0000

    Deploying to gh-pages from @ svdcvt/svdcvt.github.io@aa9874014ba27ac87a7bb86fd64234adbf207c9c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "(RECOMMENDED) Find the detailed version in PDF on the right ---&gt;",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
